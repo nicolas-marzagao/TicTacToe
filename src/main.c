f15eb34b2e15916e0dfa968b4517ac2665ca64f6
nmarzagao <nmarzagaodev@gmail.com>
2024-09-09 20:07:57 -0300

    Working Colliders

--- src/main.c
+++ src/main.c
@@ -16,14 +16,41 @@ typedef struct Board {
 	CellState cells[NUMBEROFCELLS][NUMBEROFCELLS];
 } Board;
 
-bool topRightCollision(Vector2 mousePosition) {
+bool firstRowFirstCellCollision(Vector2 mousePosition) {
 	return mousePosition.x < 200 && mousePosition.y < 200 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
 }
 
-bool topMiddleCollision(Vector2 mousePosition) {
+bool firstRowSecondCellCollision(Vector2 mousePosition) {
 	return mousePosition.x > 200 && mousePosition.x < 400 && mousePosition.y < 200 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
 }
 
+bool firstRowThirdCellCollision(Vector2 mousePosition) {
+	return mousePosition.x > 400 && mousePosition.y < 200 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+}
+
+bool secondRowFirstCellCollision(Vector2 mousePosition) {
+	return mousePosition.x < 200 && mousePosition.y > 200 && mousePosition.y < 400 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+}
+
+bool secondRowSecondCellCollision(Vector2 mousePosition) {
+	return mousePosition.x > 200 && mousePosition.x < 400 && mousePosition.y > 200 && mousePosition.y < 400 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+}
+
+bool secondRowThirdCellCollision(Vector2 mousePosition) {
+	return mousePosition.x > 400 && mousePosition.y > 200 && mousePosition.y < 400 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+}
+
+bool thirdRowFirstCellCollision(Vector2 mousePosition) {
+	return mousePosition.x < 200 && mousePosition.y > 400 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+}
+
+bool thirdRowSecondCellCollision(Vector2 mousePosition) {
+	return mousePosition.x > 200 && mousePosition.x < 400 && mousePosition.y > 400 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+}
+
+bool thirdRowThirdCellCollision(Vector2 mousePosition) {
+	return mousePosition.x > 400 && mousePosition.y > 400 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+}
 
 void drawBoard() {
 	// Vertical Lines
@@ -41,12 +68,38 @@ void drawBoard() {
 
 void squareColliders(Board* board, Vector2 mousePosition) {
 	// First Row
-	if (topRightCollision(mousePosition)) {
+	if (firstRowFirstCellCollision(mousePosition)) {
 		DrawCircle(100, 100, 10, RED);  
 	}
-	if (topMiddleCollision(mousePosition)) {
+	if (firstRowSecondCellCollision(mousePosition)) {
 		DrawCircle(300, 100, 10, RED);  
 	}
+	if (firstRowThirdCellCollision(mousePosition)) {
+		DrawCircle(500, 100, 10, RED);
+	}
+
+	// Second Row
+	if (secondRowFirstCellCollision(mousePosition)) {
+		DrawCircle(100, 300, 10, RED);
+	}
+	if (secondRowSecondCellCollision(mousePosition)) {
+		DrawCircle(300, 300, 10, RED);
+	}
+	if (secondRowThirdCellCollision(mousePosition)) {
+		DrawCircle(500, 300, 10, RED);
+	}
+
+	// Third Row
+	if (thirdRowFirstCellCollision(mousePosition)) {
+		DrawCircle(100, 500, 10, RED);
+	}
+	if (thirdRowSecondCellCollision(mousePosition)) {
+		DrawCircle(300, 500, 10, RED);
+	}
+	if (thirdRowThirdCellCollision(mousePosition)) {
+		DrawCircle(500, 500, 10, RED);
+	}
+	
 }
 
 int main () {
